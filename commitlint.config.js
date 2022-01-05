ff925d55946feae0207ee757c60ba52ada7f637a
alekseytsvetkov <code.aleksey@icloud.com>
2022-01-05 13:56:53 +0300

    chore(mobile): init husky and commitizen

---
+++ commitlint.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "scope-case": [2, "always", "lower-case"],
+    "subject-empty": [2, "never"],
+    "subject-full-stop": [2, "never", "."],
+    "type-case": [2, "always", "lower-case"],
+    "type-empty": [2, "never"],
+    "scope-enum": [2, "always", ["mobile"]],
+  },
+};
