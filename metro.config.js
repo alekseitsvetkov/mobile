0a7e4d57e6abae1194298a7500d750dbf53d3f3a
Aleksey Tsvetkov <code.aleksey@icloud.com>
2022-03-02 17:42:45 +0300

    updated metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,17 @@
-const {getDefaultConfig} = require('metro-config');
-const {resolver: defaultResolver} = getDefaultConfig.getDefaultValues();
-exports.resolver = {
-    ...defaultResolver,
-    sourceExts: [...defaultResolver.sourceExts, 'cjs'],
-};
+const {getDefaultConfig} = require('expo/metro-config');
+
+module.exports = (async () => {
+    const config = await getDefaultConfig(__dirname);
+
+    const {transformer, resolver} = config;
+
+    config.transformer = {
+        ...transformer,
+    };
+    config.resolver = {
+        ...resolver,
+        sourceExts: [...resolver.sourceExts, 'cjs'],
+    };
+
+    return config;
+})();
